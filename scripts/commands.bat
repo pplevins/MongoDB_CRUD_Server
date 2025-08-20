31c22a2c471c23a1f4a16945c1dbcff5aa1f0a8d
Pinni Plevinski <shareytora007@gmail.com>
2025-08-20 19:42:13 +0300

    Adding all yaml files and commands required for OpenShift deployment.

--- scripts/commands.bat
+++ scripts/commands.bat
@@ -4,3 +4,16 @@ docker run -d --name my-mongodb -p 27017:27017 mongo
 @REM Building docker image of the api server code and pushing it to DockerHub
 docker build -t pplevins/mongo_api_server:v1.0 .
 docker push pplevins/mongo_api_server:v1.0
+docker image tag pplevins/mongo_api_server:v1.0 pplevins/mongo_api_server:latest
+docker push pplevins/mongo_api_server:latest
+
+@REM Login to OpenShift
+oc login --token=<my-api-token> --server=https://api.rm3.7wse.p1.openshiftapps.com:6443
+oc apply -f mongo-secret.yaml
+oc apply -f mongo-pvc.yaml
+oc apply -f mongo-deployment.yaml
+oc apply -f mongo-service.yaml
+
+oc apply -f api-server-deployment.yaml
+oc apply -f api-server-service.yaml
+oc expose svc/mongo-server
