b80d104911c308b20d87f9655ef1fb78ca6c1c9b
Pinni Plevinski <shareytora007@gmail.com>
2025-08-20 16:11:17 +0300

    Adding docker build & push commands to commands.bat

--- scripts/commands.bat
+++ scripts/commands.bat
@@ -1,2 +1,6 @@
 @REM Setting local MongoDB docker container for stage 1
 docker run -d --name my-mongodb -p 27017:27017 mongo
+
+@REM Building docker image of the api server code and pushing it to DockerHub
+docker build -t pplevins/mongo_api_server:v1.0 .
+docker push pplevins/mongo_api_server:v1.0
